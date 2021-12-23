3ea5c7bddf844b2f52e5669eb87db63863f0450d
Alex Rozov <fog545@mail.ru>
2021-12-23 17:23:36 +0400

    added virtal class for Grid

---
+++ DrawCells.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fog54 on 23.12.2021.
+//
+
+#include "DrawCells.h"
---
+++ DrawCells.h
@@ -0,0 +1,14 @@
+//
+// Created by fog54 on 23.12.2021.
+//
+
+#ifndef UNTITLED2_DRAWCELLS_H
+#define UNTITLED2_DRAWCELLS_H
+
+
+class DrawCells {
+
+};
+
+
+#endif //UNTITLED2_DRAWCELLS_H
